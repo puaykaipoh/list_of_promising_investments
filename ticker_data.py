a34c2ee7ca1cf8393c96edf9e81a83b2363829c0
puaykai poh <puaykaipoh@gmail.com>
2020-03-18 22:56:19 +0800

    got statistics data

--- ticker_data.py
+++ ticker_data.py
@@ -4,7 +4,7 @@ from json import loads
 import locale
 from time import mktime
 from urllib.parse import urlencode
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 
 from mlogging import log
 
@@ -12,6 +12,7 @@ from mlogging import log
 class Ticker():
 	BASE_URL = 'https://query1.finance.yahoo.com'
 	SCRAPE_URL = 'https://finance.yahoo.com/quote'
+	SG_SCRAPE_URL = 'https://sg.finance.yahoo.com/quote'
 	NUMBER_OF_RETRY_CALLS = 8
 
 	def __init__(self, ticker):
@@ -93,6 +94,38 @@ class Ticker():
 		#return financial_parser.datum
 		return financial_parser.datum
 
+	def get_statistics_data(self):
+		locale.setlocale(locale.LC_ALL, 'en_US.UTF8')
+		url = '{}/{}/key-statistics'.format(self.SG_SCRAPE_URL, self.ticker)
+		req = Request(
+			url,
+			data=None,
+			headers={
+				"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+				"accept-encoding":"gzip, deflate, br",
+				"accept-language":"en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7",
+				"sec-fetch-dest":"document",
+				"sec-fetch-mode":"navigate",
+				"sec-fetch-site":"none",
+				"sec-fetch-user":"?1",
+				"upgrade-insecure-requests":"1",
+				"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+			}
+		)
+		print(url)
+		req = urlopen(url)
+		content = req.read().decode()
+		################
+		file = open('D:\\random\\list_of_promising_investments\\test\\sample.html', 'w')
+		file.write(content)
+		file.close()
+		################
+		statistics_parser = StatisticsParser()
+		statistics_parser.feed(content)
+		statistics = statistics_parser.datum
+		#print(statistics)
+		return statistics
+
 
 class FinancialParser(HTMLParser): #https://finance.yahoo.com/quote/Z74.SI/financials?p=Z74.SI
 	IN_TABLE = False
@@ -153,9 +186,86 @@ class FinancialParser(HTMLParser): #https://finance.yahoo.com/quote/Z74.SI/finan
 					self.datum[self.HEADERS[self.CELL_INDEX]] = existing
 			self.CELL_INDEX += 1
 
+
+class StatisticsParser(HTMLParser):
+	IN_TABLE = False
+	IN_TABLE_TABLE_COUNT = 0
+	IN_HEADER = True
+	HEADERS = {}
+	HEADER_CELL_INDEX = 0
+	BODY_CELL_INDEX = 0
+	ROW_HEADER = None
+	VALUATION_MEASURES_DATUM = {}
+	datum = {}
+	GET_TITLE = False
+	GET_SUBTITLE = False
+	TITLE = None
+	SUBTITLE = None
+
+	def handle_starttag(self, tag, attrs):
+		attrs = dict(attrs)
+		if self.IN_TABLE:
+			if tag == 'thead':
+				self.IN_HEADER = True
+		elif tag == 'table' and 'W(100%) Bdcl(c)' in attrs.get('class', []):
+			self.IN_TABLE = True
+			self.IN_TABLE_TABLE_COUNT += 1
+		elif tag == 'h2':
+			self.GET_TITLE = True
+		elif tag == 'h3':
+			self.GET_SUBTITLE = True
+
+	def handle_endtag(self, tag):
+		if self.IN_TABLE:
+			if tag == 'table':
+				self.IN_TABLE_TABLE_COUNT -= 1
+				if self.IN_TABLE_TABLE_COUNT == 0:
+					self.IN_TABLE = False
+			elif tag == 'thead':
+				self.IN_HEADER = False
+			elif tag == 'tr':
+				self.BODY_CELL_INDEX = 0
+				self.HEADER_CELL_INDEX = 0
+		
+	def handle_data(self, data):
+		if self.GET_TITLE:
+			self.TITLE = data
+			self.GET_TITLE = False
+			self.datum[self.TITLE] = {}
+		elif self.GET_SUBTITLE:
+			self.SUBTITLE = data
+			self.GET_SUBTITLE = False
+			self.datum[self.TITLE][self.SUBTITLE] = {}
+		if self.IN_TABLE:
+			#print(self.TITLE)
+			if self.TITLE == '   ####Valuation Measures': #TODO does not work
+				#print(self.BODY_CELL_INDEX, data)
+				if self.IN_HEADER:
+					#print(self.HEADERS, data, self.HEADER_CELL_INDEX)
+					self.HEADERS[self.HEADER_CELL_INDEX] = data
+					self.HEADER_CELL_INDEX += 1
+				else:
+					if self.BODY_CELL_INDEX == 0:
+						self.ROW_HEADER = data
+					else:
+						existing = self.datum[self.TITLE].get(self.HEADERS[self.BODY_CELL_INDEX], {})
+						existing[self.ROW_HEADER] = data
+						self.datum[self.TITLE][self.HEADERS[self.BODY_CELL_INDEX]] = existing
+			elif self.TITLE in ['Valuation measures','Financial highlights', 'Trading information']:
+				#print(self.BODY_CELL_INDEX, data)
+				if self.BODY_CELL_INDEX == 0:
+					self.ROW_HEADER = data
+				else:
+					if self.SUBTITLE:
+						self.datum[self.TITLE][self.SUBTITLE][self.ROW_HEADER] = data
+					else:
+						self.datum[self.TITLE][self.ROW_HEADER] = data
+			self.BODY_CELL_INDEX += 1
+
+
 if __name__ == "__main__":
 	ticker = Ticker('Z74.SI')
-	financial_data = ticker.get_financial_data()
+	financial_data = ticker.get_statistics_data()#ticker.get_financial_data()
 	import pprint
 	pp = pprint.PrettyPrinter(indent=4)
 	pp.pprint(financial_data)
