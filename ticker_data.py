d698bde0e9458fe421c85bf101237f62edc4bfa4
puaykai poh <puaykaipoh@gmail.com>
2020-04-02 21:40:34 +0800

    add headers

--- ticker_data.py
+++ ticker_data.py
@@ -24,7 +24,23 @@ class Ticker():
 		counter = 0
 		while counter < self.NUMBER_OF_RETRY_CALLS:
 			try:
-				req = urlopen(url)
+				#req = urlopen(url)
+				request = Request(
+					url,
+					data=None,
+					headers={
+						"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+						"accept-encoding":"gzip, deflate, br",
+						"accept-language":"en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7",
+						"sec-fetch-dest":"document",
+						"sec-fetch-mode":"navigate",
+						"sec-fetch-site":"none",
+						"sec-fetch-user":"?1",
+						"upgrade-insecure-requests":"1",
+						"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+					}
+				)
+				req = urlopen(request)
 				counter = self.NUMBER_OF_RETRY_CALLS
 			except:
 				counter += 1
