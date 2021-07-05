637365eddb5c9e65da68726f9549c54c2511f48a
puaykai poh <puaykaipoh@gmail.com>
2021-07-05 20:00:22 +0800

    components page got more security, pretend to be human

--- sti_components.py
+++ sti_components.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
+import gzip
 from html.parser import HTMLParser
 import os
+import random
+from time import sleep
 from urllib.request import urlopen, Request
+
+from mlogging import log
+
 
 class TableParser(HTMLParser):
   COLUMN_INDEX = None
@@ -34,10 +40,46 @@ class STIComponents():
     self.table_parser = TableParser()
     self.url = 'https://sg.finance.yahoo.com/quote/%5ESTI/components?p=%5ESTI'
     self.sgx_url = 'https://www.sgx.com/indices/products/sti'
+    self.NUMBER_OF_RETRY_CALLS = 8
 
   def get(self):
-    req = urlopen(self.url)
-    self.table_parser.feed(req.read().decode())
+    #req = urlopen(self.url)
+
+    counter = 0
+    while counter < self.NUMBER_OF_RETRY_CALLS:
+      try:
+        sleep(3*(random.random()+1.5))
+        #req = urlopen(url)
+        request = Request(
+          self.url,
+          data=None,
+          headers={
+            "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-encoding":"gzip, deflate, br",
+            "accept-language":"en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7",
+            "sec-fetch-dest":"document",
+            "sec-fetch-mode":"navigate",
+            "sec-fetch-site":"none",
+            "sec-fetch-user":"?1",
+            "upgrade-insecure-requests":"1",
+            "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+          }
+        )
+        req = urlopen(request)
+        counter = self.NUMBER_OF_RETRY_CALLS
+        data = req.read()
+      except:
+        counter += 1
+        import traceback
+        log('ERROR', 'sti_component retry times '+str(counter+1)+' error:'+traceback.format_exc())
+        sleep(3*(random.random()+1.5))
+    #print(str(req.info().get('Content-Encoding')))
+    try:
+      data = gzip.decompress(data)
+    except:
+      log('INFO', 'cannot decompress, type: '+str(req.info().get('Content-Encoding')))
+    #self.table_parser.feed(req.read().decode())
+    self.table_parser.feed(data.decode())
     return self.table_parser.datum
 
   def _get_from_sgx(self, test_env=False): # TODO incomplete and not used
